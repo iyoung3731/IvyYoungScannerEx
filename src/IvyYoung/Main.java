05f7b1d875567a95271afbcb342dcdca17a3d9db
IvyYoung <iyoung3731@bths.edu>
2018-10-15 17:18:15 -0400

    Initial commit

---
+++ src/IvyYoung/Main.java
@@ -0,0 +1,14 @@
+package IvyYoung;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = input.nextLine();
+        System.out.println("Hello "+ name + ", nice to meet you!");
+    }
+}
